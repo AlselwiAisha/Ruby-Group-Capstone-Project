4d4639e2052d918c926ed8393220fc32849e6f10
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-11-21 20:30:15 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ def main
   app = App.new
   loop do
     app.display_menu
-    option = gets.chomp
+    option = gets.to_i
     if option == '10'
       puts 'Thank you for using this app!'
       break
