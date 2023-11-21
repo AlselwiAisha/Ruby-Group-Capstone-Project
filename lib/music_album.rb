f5e41aeafb945da1e236f7a036b877a5a2307498
HenaryKh <henarykhu123@gmail.com>
2023-11-21 20:22:05 +0530

    Add music_album.rb

---
+++ lib/music_album.rb
@@ -0,0 +1,15 @@
+require 'date'
+require_relative 'item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(publish_date, on_spotify: false)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && @on_spotify == true
+  end
+end
