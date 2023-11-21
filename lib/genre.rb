08adf8412e0c9dff1ba9d3809e15e8f5a031fc63
HenaryKh <henarykhu123@gmail.com>
2023-11-21 20:34:28 +0530

    Fix linter

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,15 +1,16 @@
+# Genre
+
 class Genre
-    attr_accessor :id, :name, :items
-  
-    def initialize(name)
-      @id = Random.rand(1..1000)
-      @name = name
-      @items = []
-    end
-  
-    def add_item(item)
-      @items << item
-      item.genre = self
-    end
+  attr_accessor :id, :name, :items
+
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
   end
-  
+
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
+end
