7a0828f1263fc2a2ca910caea1261d9360e31be1
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-11-22 10:27:02 +0300

    Create Book class with publisher and cover_state

---
+++ lib/book.rb
@@ -0,0 +1,16 @@
+# book.rb
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publish_date, publisher, cover_state, archived: false)
+    super(publish_date, archived: archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
