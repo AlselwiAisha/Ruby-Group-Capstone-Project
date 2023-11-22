5aa6ac56a6c47dced262e0b2f26f3867e15a0794
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-11-22 12:18:58 +0300

    Books and Label data save added

--- app.rb
+++ app.rb
@@ -1,16 +1,26 @@
+# app.rb
 require_relative 'data_manger/game_data_manger'
 require_relative 'data_manger/author_data_manger'
+require_relative 'data_manger/book_data_manager'
+require_relative 'data_manger/label_data_manager'
 
 class App
   include GameDataManger
+  include BookDataManager
+  include LabelDataManager
+
   def initialize
     @games = GameDataManger.load_games
     @authors = AuthorDataManger.load_authors
+    @books = BookDataManager.load_books
+    @labels = LabelDataManager.load_labels
   end
 
   def save_data
     GameDataManger.save_game(@games)
     AuthorDataManger.save_author(@authors)
+    BookDataManager.save_books(@books)
+    LabelDataManager.save_labels(@labels)
   end
 
   def list_games
@@ -21,6 +31,39 @@ class App
     @authors.each { |author| puts "Author first name: #{author.first_name}, Author last name: #{author.last_name}" }
   end
 
+  def list_books
+    @books.each { |book| puts "Book ID: #{book.id}, Publisher: #{book.publisher}, Cover State: #{book.cover_state}, Archived: #{book.archived}" }
+  end
+
+  def add_book
+    print 'Enter publish date (YYYY-MM-DD): '
+    publish_date = gets.chomp
+
+    print 'Enter publisher: '
+    publisher = gets.chomp
+
+    print 'Enter cover state: '
+    cover_state = gets.chomp
+
+    book = Book.new(publish_date, publisher, cover_state)
+    @books << book
+
+    create_label(book)
+    puts 'Book added successfully!'
+  end
+
+  def create_label(item)
+    print 'Enter label title: '
+    title = gets.chomp
+
+    print 'Enter label color: '
+    color = gets.chomp
+
+    label = Label.new(title, color)
+    label.add_item(item)
+    @labels << label
+  end
+
   def display_menu
     puts ' '
     puts '*' * 50
@@ -28,7 +71,7 @@ class App
     puts '*' * 50
     puts 'Please choose an option by entering a number:'
     puts '1 - List all books'
-    puts '2 - List all music album'
+    puts '2 - List all music albums'
     puts '3 - List all games'
     puts '4 - List all genres'
     puts '5 - List all labels'
@@ -42,13 +85,13 @@ class App
   def process_option
     option = gets.chomp.to_i
     options = {
-      1 => -> { 'list_all_books' },
+      1 => -> { list_books },
       2 => -> { 'list_all_music_albums' },
       3 => -> { list_games },
       4 => -> { 'list_all_genres' },
       5 => -> { 'list_all_labels' },
       6 => -> { list_authors },
-      7 => -> { 'add_book' },
+      7 => -> { add_book },
       8 => -> { 'add_music_album' },
       9 => -> { create_game },
       10 => lambda {
--- lib/book.rb
+++ lib/book.rb
@@ -11,6 +11,17 @@ class Book < Item
   end
 
   def can_be_archived?
-    super || @cover_state == 'bad'
+    super || @cover_state == "bad"
+  end
+
+  def to_hash
+    {
+      id: @id,
+      publish_date: @publish_date,
+      archived: @archived,
+      label: @label&.to_hash,
+      publisher: @publisher,
+      cover_state: @cover_state
+    }
   end
 end
