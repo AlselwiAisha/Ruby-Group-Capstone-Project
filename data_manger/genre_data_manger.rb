71f3a9486f9abfc653548bfcf212a88c43dd4372
HenaryKh <henarykhu123@gmail.com>
2023-11-22 22:13:00 +0530

    Add genre data manger

---
+++ data_manger/genre_data_manger.rb
@@ -0,0 +1,23 @@
+require_relative '../lib/genre'
+
+module GenreModule
+
+  def self.load_genres
+    if File.exist?('./data/genre.json')
+      genres = JSON.parse(File.read('./data/genre.json')).map do |genre|
+        Genre.new(genre['Genre_name'])
+      end
+    else
+      File.write('./data/genre.json', JSON.dump([]))
+    end
+    genres.any? ? genres : []
+  end
+
+  def self.save_genre(genres)
+    if genres.any?
+      File.write('./data/genre.json', JSON.dump(genres))
+    else
+      File.write('./data/genre.json', JSON.dump([]))
+    end
+  end
+end
