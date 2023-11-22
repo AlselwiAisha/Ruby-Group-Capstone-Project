21aa1d6e338bb3aa3e3ce357ed27c6c50dbdaba3
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-11-22 10:28:47 +0300

    Add save_data and list_games

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
-require_relative './data_manger/game_data_manger'
-require 'pry'
+require_relative 'data_manger/game_data_manger'
+
 class App
-   include GameDataManger 
-   def initialize
-    @games = []
+  include GameDataManger
+  def initialize
+    @games = GameDataManger.load_games
     @authors = []
-   end10
+  end
+
+  def save_data
+    GameDataManger.save_game(@games)
+  end
+
+  def list_games
+    @games.each { |game| puts "Multiplayer: #{game.multiplayer}, Last played at: #{game.last_played_at}" }
+  end
+
   def display_menu
     puts ' '
     puts '*' * 50
@@ -24,20 +33,25 @@ class App
     puts '10 - Exit'
   end
 
-  def process_option(option)
+  def process_option
+    option = gets.chomp.to_i
     options = {
       1 => -> { 'list_all_books' },
       2 => -> { 'list_all_music_albums' },
-      3 => -> { 'list_all_games' },
+      3 => -> { list_games },
       4 => -> { 'list_all_genres' },
       5 => -> { 'list_all_labels' },
       6 => -> { 'list_all_authors' },
       7 => -> { 'add_book' },
       8 => -> { 'add_music_album' },
-      9 => -> { create_game }
+      9 => -> { create_game },
+      10 => lambda {
+        save_data
+        exit
+      }
     }
     case option
-    when 1, 2, 3, 4, 5, 6, 7, 8, 9
+    when 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
       options[option].call
     end
   end
